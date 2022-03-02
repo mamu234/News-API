f1ad5fe948708774f72a4d65491eec964a452da1
carolyne <pcmaunda@gmail.com>
2022-03-02 11:51:59 +0300

    added search results

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,8 @@
 from app import app, render_template
 from templates.requests import News, get_news
-from templates.requests import get_news,get_news
+from templates.requests import get_news,get_news,search_news
+
+
 
 # Views
 @app.route('/news/<int:id>')
@@ -8,6 +10,14 @@ def news(id):
     news = get_news(id)
     title = f'{news.title}'
 
+@app.route('/search/<news_name>')
+def search(news_name):
+
+
+ news_name_list = news_name.split(" ")
+ news_name_format = "+".join(news_name_list)
+ searched_news = search_news(news_name_format)
+ title = f'search results for {news_name}'
     
-    return render_template('news.html',title = title,news = news)
+ return render_template('search.html',movies = searched_news)
 
--- app/templates/requests.py
+++ app/templates/requests.py
@@ -22,24 +22,34 @@ def get_news(id):
     get_news_url = base_url.format(id,api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
-          news_details_data = url.read()
-          news_details_response = json.loads(news_details_data)
+      news_details_data = url.read()
+      news_details_response = json.loads(news_details_data)
 
-          news_object = None
-          if news_details_response:
-            id = news_details_response.get('id')
-            name = news_details_response.get('original_title')
-            description = news_details_response.get('overview')
-            poster = news_details_response.get('poster_path')
-            category= news_details_response.get('vote_average')
-            langauge = news_details_response.get('vote_count')
-            country = news_details_response.response('country')
+      news_object = None
+      if news_details_response:
+          id = news_details_response.get('id')
+          name = news_details_response.get('original_title')
+          description = news_details_response.get('overview')
+          poster = news_details_response.get('poster_path')
+          category= news_details_response.get('vote_average')
+          langauge = news_details_response.get('vote_count')
+          country = news_details_response.response('country')
 
           news_object = News(id,name,description,poster, category, langauge,country)
     
+def search_news(news_name):
+    search_news_url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=6cc037b2e9984d0b995fb8d87f06d897'.format(api_key,news_name)
+    with urllib.request.urlopen(search_news_url) as url:
+        search_news_data = url.read()
+        search_news_response = json.loads(search_news_data)
 
+        search_news_results = None
+
+        if search_news_response['results']:
+            search_news_list = search_news_response['results']
+            search_news_results = search_news_results(search_news_list)
     
-    return news_object
+    return search_news_results
 
 
 
