5ee4ab55bad5ac4f8ba40dd761d042c92eb05ee5
carolyne <pcmaunda@gmail.com>
2022-03-02 06:32:33 +0300

    added bootstrap

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import DevConfig
-from flask import Blueprint
 
-from app.main import main_blueprint
-from templates.requests import configure_request
+
+
 
 bootstrap = Bootstrap()
 
 app = Flask(__name__,relative_config = True)
-    # main = Blueprint('main',__name__)
+  
 
     # Creating the app configurations
 app.config.from_object(DevConfig)
