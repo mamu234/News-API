ff60e558587e0f47b2ed0f7ed986812def8d9983
carolyne <pcmaunda@gmail.com>
2022-03-11 13:38:20 +0300

    renamed procfile

--- config.py
+++ config.py
@@ -9,3 +9,19 @@ class ProdConfig(Config):
 
     class DevConfig(Config):
      DEBUG = False
+
+  
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
